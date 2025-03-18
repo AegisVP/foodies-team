ad14e0bdbcb6f8dccc6e15292fc88c3c83868d6e
Vladislav Pysarenko <vlad@pysarenko.com>
2025-03-18 20:56:01 +0100

    placeholders (#6)
    * feature: endpoint GET /recipes
    
    * placeholders
    
    * remove list endpoint
    
    ---------
    
    Co-authored-by: Mariia Illarionova <illarionovam@gmail.com>

--- app/api/routes/recipesRouter.js
+++ app/api/routes/recipesRouter.js
@@ -6,4 +6,18 @@ const recipiesRouter = express.Router();
 recipiesRouter.get('/', listRecipes);
 recipiesRouter.get('/:id', getRecipeById);
 
+// recipiesRouter.get('/popular', recipesController); // популярні рецепти
+
+// recipiesRouter.use(authMiddleware);
+
+// recipiesRouter.post('/', recipesController); // create recipe
+// recipiesRouter.delete('/', recipesController); // delete recipe
+
+// recipiesRouter.get('/owner', recipesController); // мої рецепти
+// recipiesRouter.get('/owner/:id', recipesController); // рецепти користувача
+
+// recipiesRouter.get('/favorite', recipesController); // мої обрані
+// recipiesRouter.post('/favorite', recipesController); // додати в обрані
+// recipiesRouter.delete('/favorite', recipesController); // видалити з обраних
+
 export default recipiesRouter;
