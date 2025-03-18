b5bfb60e09ecae5d6025f0656442846d4fda3c8c
Mariia Illarionova <illarionovam@gmail.com>
2025-03-18 21:32:50 +0200

    review fixes

--- app/api/services/recipesService.js
+++ app/api/services/recipesService.js
@@ -3,17 +3,29 @@ import { User } from '../models/users.js';
 import { sequelize } from '../db/db.js';
 import { QueryTypes } from 'sequelize';
 
-async function listRecipes(limit, offset, whereCondition = null) {
-    return await sequelize.query(
+async function listRecipes(limit = 12, page = 1, whereCondition = null) {
+    const recipes = await sequelize.query(
         `SELECT "recipes".*, 
                 "user"."id" AS "user_id", 
                 "user"."name" AS "user_name", 
                 "user"."avatar" AS "user_avatar", 
                 "user"."email" AS "user_email" FROM "recipes" AS "recipes" LEFT OUTER JOIN "users" AS "user" ON "recipes"."owner" = "user"."id" ${
                     whereCondition ? `WHERE ${whereCondition}` : ''
-                } LIMIT ${limit} OFFSET ${offset}`,
+                } LIMIT ${limit} OFFSET ${(page - 1) * limit}`,
         { type: QueryTypes.SELECT }
     );
+
+    return recipes.map((recipe) => ({
+        id: recipe.id,
+        owner: {
+            id: recipe.user_id,
+            name: recipe.user_name,
+            avatar: recipe.user_avatar,
+        },
+        title: recipe.title,
+        description: recipe.description,
+        thumb: recipe.thumb,
+    }));
 }
 
 async function getRecipeById(id, ownerAttributes = ['id', 'name', 'avatar']) {
