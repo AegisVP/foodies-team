168425640aa47b3ec022ff51bb180dfb30fb58d1
Mariia Illarionova <illarionovam@gmail.com>
2025-03-20 21:09:06 +0200

    fixed bug with association on recipes/id endpoint

--- app/api/services/recipesService.js
+++ app/api/services/recipesService.js
@@ -2,6 +2,8 @@ import { Recipe } from '../models/recipes.js';
 import { User } from '../models/users.js';
 import { sequelize } from '../db/db.js';
 import { QueryTypes } from 'sequelize';
+import { Category } from '../models/categories.js';
+import { Area } from '../models/areas.js';
 
 async function listRecipes(limit = 12, page = 1, whereCondition = null) {
     const recipes = await sequelize.query(
@@ -35,6 +37,14 @@ async function getRecipeById(id, ownerAttributes = ['id', 'name', 'avatar']) {
                 attributes: ownerAttributes,
                 model: User,
                 as: 'user',
+            },
+            {
+                model: Category,
+                as: 'category_association',
+            },
+            {
+                model: Area,
+                as: 'area_association',
             },
         ],
     });
