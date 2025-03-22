89b8562a67cc8dd8c276046eac68da5a6bc26cc7
Karpenko Nataliia <48415878+Netaly79@users.noreply.github.com>
2025-03-22 20:52:59 +0100

    add meal card

---
+++ app/webfront/src/components/MealCard/index.jsx
@@ -0,0 +1,29 @@
+import css from './MealCard.module.css';
+import heart from '../../images/heart.svg';
+import arrow from '../../images/arrow.svg';
+
+const MealCard = ({ mealImage, title, description, userAvatar, userName }) => {
+    return (
+        <div className={css.card}>
+            <img src={mealImage} alt="meal" className={css.cardImg} />
+            <h2 className={css.cardTitle}>{title}</h2>
+            <p className={css.cardDescription}>{description}</p>
+            <div className={css.cardFooter}>
+                <div className={css.userInfo}>
+                    <img src={userAvatar} alt="user" className={css.userImg} />
+                    <p className={css.userName}>{userName}</p>
+                </div>
+                <div className={css.cardActions}>
+                    <button className={css.cardBtn}>
+                        <img src={heart} alt="favorite" />
+                    </button>
+                    <button className={css.cardBtn}>
+                        <img src={arrow} alt="details" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MealCard;
