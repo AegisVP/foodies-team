bfa2e5e8c9743cdf00f827002fee4e5a247c62da
Pasha Matchak <pavlo@chess.com>
2025-03-23 12:14:09 +0200

    cleanup

--- app/webfront/src/pages/HomePage/index.jsx
+++ app/webfront/src/pages/HomePage/index.jsx
@@ -2,8 +2,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useShowError } from 'src/hooks/useShowError.js';
 import { selectCategories, selectCommonError, selectIsCommonLoading } from 'src/redux/common/selectors';
 import { getCategories } from 'src/redux/common/operations';
-import Button from 'src/components/Button';
-import spriteFacebook from '../../images/icons.svg#facebook';
 
 const HomePage = () => {
     const isCategoriesLoading = useSelector(selectIsCommonLoading);
@@ -24,18 +22,6 @@ const HomePage = () => {
 
             <p>Categories:</p>
             <button onClick={handleGetCategories}>Get categories</button>
-
-            <Button label="Test" />
-
-            <Button label="Test" theme="dark" />
-
-            <Button label="Test" href="/test" />
-
-            <Button label="Test" fullWidth />
-
-            <Button label="Test" icon={spriteFacebook} />
-
-            <Button icon={spriteFacebook} to='/' />
 
             {isCategoriesLoading && <p>Loading getCategories...</p>}
             {!isCategoriesLoading && !!categories?.length && (
