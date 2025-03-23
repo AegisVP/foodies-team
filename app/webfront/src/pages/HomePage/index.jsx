9725d6bcaf2d85c649e13118026283330648bc85
Pasha Matchak <pavlo@chess.com>
2025-03-23 12:12:22 +0200

    Base buttons

--- app/webfront/src/pages/HomePage/index.jsx
+++ app/webfront/src/pages/HomePage/index.jsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useShowError } from 'src/hooks/useShowError.js';
 import { selectCategories, selectCommonError, selectIsCommonLoading } from 'src/redux/common/selectors';
 import { getCategories } from 'src/redux/common/operations';
+import Button from 'src/components/Button';
+import spriteFacebook from '../../images/icons.svg#facebook';
 
 const HomePage = () => {
     const isCategoriesLoading = useSelector(selectIsCommonLoading);
@@ -22,6 +24,19 @@ const HomePage = () => {
 
             <p>Categories:</p>
             <button onClick={handleGetCategories}>Get categories</button>
+
+            <Button label="Test" />
+
+            <Button label="Test" theme="dark" />
+
+            <Button label="Test" href="/test" />
+
+            <Button label="Test" fullWidth />
+
+            <Button label="Test" icon={spriteFacebook} />
+
+            <Button icon={spriteFacebook} to='/' />
+
             {isCategoriesLoading && <p>Loading getCategories...</p>}
             {!isCategoriesLoading && !!categories?.length && (
                 <ul>
