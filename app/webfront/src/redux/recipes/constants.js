37baab0f67c4f752124a541fe5f26589a6566576
Vitalii Kletsko <v.kletsko@gmail.com>
2025-03-28 00:09:32 +0200

    feat: profile own recipes (#50)
    * feat: profile own recipes
    
    * fix: recipeOwnerCard container
    
    * fix: RecipeOwnerCard shadows
    
    * fix: typograpy in RecipeOwnerCard
    
    ---------
    
    Co-authored-by: Vitalii Kletsko <vitalii.kletsko@Cole.local>

--- app/webfront/src/redux/recipes/constants.js
+++ app/webfront/src/redux/recipes/constants.js
@@ -1,3 +1,4 @@
 export const FETCH_RECIPES = 'recipes/fetchRecipes';
+export const FETCH_OWNER_RECIPES = 'recipes/fetchOwnerRecipes';
 export const ADD_RECIPE = 'recipes/addRecipe';
 export const DELETE_RECIPE = 'recipes/deleteRecipe';
