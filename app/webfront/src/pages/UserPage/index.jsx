1e4daca60c21513971fa009b82a48ff90f646102
Vladislav Pysarenko <vlad@pysarenko.com>
2025-03-27 03:16:01 +0100

    user page (WiP)

--- app/webfront/src/pages/UserPage/index.jsx
+++ app/webfront/src/pages/UserPage/index.jsx
@@ -1,41 +1,65 @@
-import { Suspense } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import ROUTES from 'src/navigation/routes.js';
+import { Suspense, useEffect } from 'react';
+import { NavLink, Outlet, useParams } from 'react-router-dom';
 
+import ROUTES from 'src/navigation/routes.js';
+import css from './UserPage.module.css';
 import { Loader } from 'src/components';
 import UserProfileCard from 'src/components/UserProfile/UserProfileCard/UserProfileCard';
-import { useDispatch } from 'react-redux';
+import { getUserInformation } from 'src/api/user';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectAuthUser } from 'src/redux/authUser/selectors';
+import { setProfile } from 'src/redux/user/slice';
+import { selectUserProfile } from 'src/redux/user/selectors';
 
 const UserPage = () => {
     const dispatch = useDispatch();
-    const user = dispatch(selectAuthUser);
-    console.log({ user });
-    return (
-        <div>
-            <h1>Profile</h1>
-            <p>Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us. </p>
-            <UserProfileCard />
-            <span>[logout button]</span>
+    const { id } = useParams();
+    const userProfile = useSelector(selectUserProfile);
+    const authUser = useSelector(selectAuthUser);
+    const authUserId = authUser?.id;
+    const ownUser = userProfile?.id === authUserId;
+
+    useEffect(() => {
+        if (!id && !authUserId) return;
+
+        getUserInformation(id ?? authUserId).then(data => {
+            dispatch(setProfile(data));
+        });
+    }, [authUserId, dispatch, id]);
+
+    return userProfile ? (
+        <div className={css.container}>
+            <h1 className={css.title}>Profile</h1>
+            <p className={css.description}>
+                Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us.{' '}
+            </p>
+            <UserProfileCard userProfile={userProfile} />
+            {ownUser ? <span>[logout button]</span> : <span>[follow button]</span>}
             <ul style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
                 <li>
-                    <Link to={ROUTES.RECIPES}>My Recipes</Link>
+                    <NavLink to={ROUTES.RECIPES}>My Recipes</NavLink>
                 </li>
+                {ownUser && (
+                    <li>
+                        <NavLink to={ROUTES.FAVORITES}>My Favorites</NavLink>
+                    </li>
+                )}
                 <li>
-                    <Link to={ROUTES.FAVORITES}>My Favorites</Link>
+                    <NavLink to={ROUTES.FOLLOWERS}>Followers</NavLink>
                 </li>
-                <li>
-                    <Link to={ROUTES.FOLLOWERS}>Followers</Link>
-                </li>
-                <li>
-                    <Link to={ROUTES.FOLLOWING}>Following</Link>
-                </li>
+                {ownUser && (
+                    <li>
+                        <NavLink to={ROUTES.FOLLOWING}>Following</NavLink>
+                    </li>
+                )}
             </ul>
 
             <Suspense fallback={<Loader />}>
                 <Outlet />
             </Suspense>
         </div>
+    ) : (
+        <Loader />
     );
 };
 
