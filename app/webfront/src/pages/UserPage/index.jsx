bb12fbde39158457399fbdaaf9b251614a1ff33e
Pasha Matchak <pavlo@chess.com>
2025-03-27 18:05:08 +0200

    tab reset, user profile link

--- app/webfront/src/pages/UserPage/index.jsx
+++ app/webfront/src/pages/UserPage/index.jsx
@@ -1,13 +1,18 @@
 import clsx from 'clsx';
-import { Suspense, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Suspense, useState, useEffect } from 'react';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import ROUTES from 'src/navigation/routes.js';
 import css from './UserPage.module.css';
 
 import { Loader } from 'src/components';
 
 const UserPage = () => {
-    const [activetab, setActiveTab] = useState('recipes');
+    const { id } = useParams();
+    const [activeTab, setActiveTab] = useState('recipes');
+
+    useEffect(() => {
+        setActiveTab('recipes');
+    }, [id]);
 
     return (
         <div className={css.component}>
@@ -18,16 +23,16 @@ const UserPage = () => {
 
             <div>
                 <ul className={css.tabs}>
-                    <li className={clsx(css.tab, { [css.active]: activetab === 'recipes' })}>
+                    <li className={clsx(css.tab, { [css.active]: activeTab === 'recipes' })}>
                         <Link to={ROUTES.RECIPES} onClick={() => setActiveTab('recipes')}>My Recipes</Link>
                     </li>
-                    <li className={clsx(css.tab, { [css.active]: activetab === 'favorites' })}>
+                    <li className={clsx(css.tab, { [css.active]: activeTab === 'favorites' })}>
                         <Link to={ROUTES.FAVORITES} onClick={() => setActiveTab('favorites')}>My Favorites</Link>
                     </li>
-                    <li className={clsx(css.tab, { [css.active]: activetab === 'followers' })}>
+                    <li className={clsx(css.tab, { [css.active]: activeTab === 'followers' })}>
                         <Link to={ROUTES.FOLLOWERS} onClick={() => setActiveTab('followers')}>Followers</Link>
                     </li>
-                    <li className={clsx(css.tab, { [css.active]: activetab === 'following' })}>
+                    <li className={clsx(css.tab, { [css.active]: activeTab === 'following' })}>
                         <Link to={ROUTES.FOLLOWING} onClick={() => setActiveTab('following')}>Following</Link>
                     </li>
                 </ul>
