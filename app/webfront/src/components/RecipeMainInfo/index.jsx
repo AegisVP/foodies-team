22cfa277458ddfd06fc3f0f9b3f43b847706842c
LesyaTkachuk <lesyalesya9876@gmail.com>
2025-03-30 20:10:53 +0300

    add redirect on login on recipe page

--- app/webfront/src/components/RecipeMainInfo/index.jsx
+++ app/webfront/src/components/RecipeMainInfo/index.jsx
@@ -1,6 +1,6 @@
 import css from './RecipeMainInfo.module.css';
 
-const RecipeMainInfo = ({ recipe }) => {
+const RecipeMainInfo = ({ recipe, onUserAvatarClick }) => {
     return (
         <div className={css.recipe}>
             <section className={css.recipePageHeader}>
@@ -10,12 +10,12 @@ const RecipeMainInfo = ({ recipe }) => {
                     <p className={css.time}>{recipe.time} min</p>
                 </div>
                 <p className={css.recipeDescription}>{recipe.category?.description}</p>
-                <div className={css.userBlock}>
+                <button className={css.userBlock} onClick={() => onUserAvatarClick(recipe.owner.id)}>
                     <img src={recipe.owner?.avatar} alt="user" className={css.userImg} />
                     <p className={css.userName}>
                         Created by: <br /> <span>{recipe.owner?.name}</span>
                     </p>
-                </div>
+                </button>
             </section>
         </div>
     );
