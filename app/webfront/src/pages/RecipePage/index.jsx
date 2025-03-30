d7632889d7938d6121d456217b24d092af6134c1
LesyaTkachuk <55761336+LesyaTkachuk@users.noreply.github.com>
2025-03-30 20:20:47 +0300

    redirect on login on recipe page (#73)
    * Revert "fix button"
    
    This reverts commit 2bd73ef18a3e5b75010455d5ee032d3aff74cf7b.
    
    * add redirect to Login for unauthorized user
    
    * Revert "fix button"
    
    This reverts commit 2bd73ef18a3e5b75010455d5ee032d3aff74cf7b.
    
    * add redirect on login on recipe page

--- app/webfront/src/pages/RecipePage/index.jsx
+++ app/webfront/src/pages/RecipePage/index.jsx
@@ -1,9 +1,46 @@
-import RecipeInfo from '../../components/RecipeInfo';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import RecipeInfo from 'src/components/RecipeInfo';
+import AuthModal from 'src/components/AuthModal';
+
+import { ROUTES } from 'src/navigation/routes';
+import { selectIsAuthenticated } from 'src/redux/authUser/selectors';
+import { useAuthHook } from 'src/hooks/useAuthHook';
+import { replaceUrlParams } from 'src/utils/replaceUrlParams';
 
 const RecipePage = ({ setCustomBreadcrumbs }) => {
+    const navigate = useNavigate();
+
+    const isAuthenticated = useSelector(selectIsAuthenticated);
+    const {
+        isLoginModalOpen,
+        isRegisterModalOpen,
+        openLoginModal,
+        closeLoginModal,
+        openRegisterModal,
+        closeRegisterModal,
+    } = useAuthHook();
+
+    const onUserAvatarClick = id => {
+        if (isAuthenticated) {
+            navigate(replaceUrlParams(ROUTES.USER_PAGE, { id }));
+        } else {
+            openLoginModal();
+        }
+    };
+
     return (
         <>
-            <RecipeInfo setCustomBreadcrumbs={setCustomBreadcrumbs} />
+            <RecipeInfo setCustomBreadcrumbs={setCustomBreadcrumbs} onUserAvatarClick={onUserAvatarClick} />
+            <AuthModal
+                isLoginModalOpen={isLoginModalOpen}
+                isRegisterModalOpen={isRegisterModalOpen}
+                openLoginModal={openLoginModal}
+                closeLoginModal={closeLoginModal}
+                openRegisterModal={openRegisterModal}
+                closeRegisterModal={closeRegisterModal}
+            />
         </>
     );
 };
