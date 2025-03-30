8fa977d302b92de650143abe2130c461423242a1
Vladislav Pysarenko <vlad@pysarenko.com>
2025-03-30 22:23:12 +0200

    alphabetical sort

--- app/api/services/categoriesServices.js
+++ app/api/services/categoriesServices.js
@@ -1,7 +1,7 @@
 import { Category } from '../models/categories.js';
 
 async function listCategories(whereCondition = null) {
-    return await Category.findAll({ where: whereCondition });
+    return await Category.findAll({ where: whereCondition, order: [['name', 'ASC']] });
 }
 
 export default {
