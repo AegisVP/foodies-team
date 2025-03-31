ff28b9d2cfd6b7f583075b5f578faa79cd999eb9
Vladislav Pysarenko <vlad@pysarenko.com>
2025-03-31 02:14:41 +0200

    add route builder

--- app/webfront/src/navigation/routes.js
+++ app/webfront/src/navigation/routes.js
@@ -10,6 +10,18 @@ export const ROUTES = {
     FOLLOWERS: 'followers',
     FOLLOWING: 'following',
     NOT_FOUND: '*',
+    BUILD: (path, query) => {
+        if (Array.isArray(path)) {
+            path = path.join('/');
+        } else if (typeof path === 'string') {
+            path = path.trim().slice(path.startsWith('/') ? 1 : 0, path.endsWith('/') ? -1 : undefined);
+        }
+        let queryArr = [];
+        for (const [key, val] in query) {
+            queryArr.push(`${key}=${val}`);
+        }
+        return `/${path}${query ? `?${queryArr.join('&')}` : ''}`;
+    },
 };
 
 export default ROUTES;
