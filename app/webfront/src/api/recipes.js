8928cda44a2dad439f9c656c7a264bda6a9fc5b9
Vladislav Pysarenko <vlad@pysarenko.com>
2025-03-31 04:48:37 +0200

    cleanup

--- app/webfront/src/api/recipes.js
+++ app/webfront/src/api/recipes.js
@@ -2,7 +2,7 @@ import axios, { HEADER_MFD } from './axiosInstance.js';
 
 export const getRecipes = async params => await axios.get('/recipes', { params });
 
-export const getOwnerRecipes = async (userId, query) => await axios.get(`/recipes/owner/${userId}?${query}`);
+export const getOwnerRecipes = async (userId, params) => await axios.get(`/recipes/owner/${userId}`, { params });
 
 export const addRecipe = async data => await axios.post('/recipes', data, { headers: HEADER_MFD });
 
